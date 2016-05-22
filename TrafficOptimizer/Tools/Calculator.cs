37541d043b72dac51c47acf65912596f4b5c1cf6
CaCTuCaTu4ECKuu <cactucatu4eckuu@gmail.com>
2016-05-23 01:37:25 +0300

    Ошибка в формуле расчёта расстояния между точками

--- TrafficOptimizer/Tools/Calculator.cs
+++ TrafficOptimizer/Tools/Calculator.cs
@@ -11,7 +11,7 @@ namespace TrafficOptimizer.Tools
     {
         public static double Length(Point p1, Point p2)
         {
-            return Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p2.Y), 2));
+            return Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p1.Y), 2));
         }
     }
 }
